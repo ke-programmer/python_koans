d243ba2b196837fbbde2fadb8ce3fc5d08e8522e
Kenny Rachuonyo <krmboya@gmail.com>
2022-03-23 04:30:16 +0300

    contemplate modules and packages

--- koans/about_packages.py
+++ koans/about_packages.py
@@ -29,13 +29,13 @@ class AboutPackages(Koan):
         from .a_package_folder.a_module import Duck
 
         duck = Duck()
-        self.assertEqual(__, duck.name)
+        self.assertEqual("Donald", duck.name)
 
     def test_subfolders_become_modules_if_they_have_an_init_module(self):
         # Import ./a_package_folder/__init__.py
         from .a_package_folder import an_attribute
 
-        self.assertEqual(__, an_attribute)
+        self.assertEqual(1984, an_attribute)
 
     # ------------------------------------------------------------------
 
@@ -43,7 +43,7 @@ class AboutPackages(Koan):
         # Import /contemplate_koans.py
         import contemplate_koans
 
-        self.assertEqual(__, contemplate_koans.__name__)
+        self.assertEqual("contemplate_koans", contemplate_koans.__name__)
 
         # contemplate_koans.py is the root module in this package because it's
         # the first python module called in koans.
@@ -52,9 +52,10 @@ class AboutPackages(Koan):
         # the root folder, which would make reaching the koans folder
         # almost impossible. So always leave the starting python script in
         # a folder which can reach everything else.
+        # KR: Won't tweaking the python path help with the above scenario?
 
     def test_import_a_module_in_a_subfolder_folder_using_an_absolute_path(self):
         # Import contemplate_koans.py/koans/a_package_folder/a_module.py
         from koans.a_package_folder.a_module import Duck
 
-        self.assertEqual(__, Duck.__module__)
+        self.assertEqual("koans.a_package_folder.a_module", Duck.__module__)
